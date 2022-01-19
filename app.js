5a561997cc994f6034b7056673d814d2cd5a60f3
sveloso12 <veloso50@hotmail.fr>
2022-01-19 04:48:33 -0800

    nouveau fichier

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/contacts', contacts);
-app.use('/VDE', VDE);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
